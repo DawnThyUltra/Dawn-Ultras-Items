4265d68ac3cd992599facea3bd3b3713e9b76a6c
DawnThyUltra <marion38361@gmail.com>
2024-03-15 23:01:32 -0400

    Added MaskedPlayerEnemyPatch to plugin patches

--- Template/Plugin.cs
+++ Template/Plugin.cs
@@ -62,5 +62,6 @@ public class Plugin : BaseUnityPlugin
     {
         _harmony.PatchAll(typeof(ShipLightsPatch));
         _harmony.PatchAll(typeof(PlayerControllerBPatch));
+        _harmony.PatchAll(typeof(MaskedPlayerEnemyPatch));
     }
 }
