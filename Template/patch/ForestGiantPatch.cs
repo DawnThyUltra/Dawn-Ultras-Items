bfed5c11741fd1e4000071f0fcb05f468664a1bc
DawnThyUltra <marion38361@gmail.com>
2024-03-16 11:07:12 -0400

    Added ForestGiantPatch

---
+++ Template/patch/ForestGiantPatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YourThunderstoreTeam.patch
+{
+    [HarmonyPatch(typeof(ForestGiantPatch))]
+    public class ForestGiantPatch
+    {
+
+    }
+}
