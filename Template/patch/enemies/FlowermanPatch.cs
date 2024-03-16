8791a1cb4d7522aae142a411a26da952f2546f7d
DawnThyUltra <marion38361@gmail.com>
2024-03-16 11:19:54 -0400

    Added FlowermanPatch and an invincibility-proof kill animation patch

---
+++ Template/patch/enemies/FlowermanPatch.cs
@@ -0,0 +1,24 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace YourThunderstoreTeam.patch.enemies
+{
+    [HarmonyPatch(typeof(FlowermanAI))]
+    public class FlowermanPatch
+    {
+        [HarmonyPatch("KillPlayerAnimationServerRpc", MethodType.Normal)]
+        [HarmonyPrefix]
+        private static bool OnKillPlayerAnimationServerRpc(ref FlowermanAI __instance)
+        {
+            PlayerControllerB? targetPlayer = __instance.targetPlayer;
+
+            if (targetPlayer is not null)
+            {
+                bool isInvincible = PlayerControllerBPatch.IsPlayerInvincible(targetPlayer);
+                return !isInvincible;
+            }
+
+            return true;
+        }
+    }
+}
