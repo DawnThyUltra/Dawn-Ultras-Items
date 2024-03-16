38a1b2115ddbad50d6e0b4bc81848f30de0fbef2
DawnThyUltra <marion38361@gmail.com>
2024-03-15 22:45:05 -0400

    Added OnKillAnimation

--- Template/patch/MaskedPlayerEnemyPatch.cs
+++ Template/patch/MaskedPlayerEnemyPatch.cs
@@ -1,13 +1,24 @@
-﻿using HarmonyLib;
+﻿using GameNetcodeStuff;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace YourThunderstoreTeam.patch
 {
-    [HarmonyPatch()]
+    [HarmonyPatch(typeof(MaskedPlayerEnemy))]
     public class MaskedPlayerEnemyPatch
     {
+        [HarmonyPatch("killAnimation", MethodType.Normal)]
+        [HarmonyPrefix]
+        private static bool OnKillAnimation(ref MaskedPlayerEnemy __instance)
+        {
+            PlayerControllerB? targetPlayer = __instance.targetPlayer;
 
+            if (targetPlayer is not null) 
+                return !PlayerControllerBPatch.IsPlayerInvincible(targetPlayer);
+
+            return true;
+        }
     }
 }
