4aeb2ada2a23b764c0a115c081c912270382e57d
DawnThyUltra <marion38361@gmail.com>
2024-04-16 07:47:42 -0400

    added EnemyAIPatch

---
+++ Template/patch/enemies/EnemyAIPatch.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YourThunderstoreTeam.patch.enemies
+{
+    [HarmonyPatch(typeof(EnemyAI))]
+    public class EnemyAIPatch
+    {
+    }
+}
