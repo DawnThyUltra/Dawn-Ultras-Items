771c4833a6480075510836cd2ec87909557b243d
DawnThyUltra <marion38361@gmail.com>
2025-02-07 11:28:49 -0500

    added temp killstreak voicelines

--- Template/patch/Items/EnergySwordItem.cs
+++ Template/patch/Items/EnergySwordItem.cs
@@ -31,10 +31,21 @@ namespace YourThunderstoreTeam.patch.Items
 
         #region Announcer
         private int _killCount = 0;
+        private int _tempKillCount = 0;
+        private float _lastKillTimestamp = Time.time;
         private Queue<AudioClip> _announcerQueue = new Queue<AudioClip>();
         private bool _announcing = false;
 
         public AudioSource AnnouncerAudioSource;
+        public AudioClip DoubleKill;
+        public AudioClip TripleKill;
+        public AudioClip Overkill;
+        public AudioClip Killtacular;
+        public AudioClip Killtrocity;
+        public AudioClip Killamanjaro;
+        public AudioClip Killtastrophe;
+        public AudioClip Killpocalypse;
+        public AudioClip Killionaire;
         public AudioClip Betrayal;
         public AudioClip KillingSpree;
         public AudioClip KillingFrenzy;
@@ -79,6 +90,15 @@ namespace YourThunderstoreTeam.patch.Items
             energySwordScript.SwordHitEnvSfx = assetBundle.LoadAsset<AudioClip>("Energy_sword_hit_env.wav");
 
             #region Announcer Lines
+            energySwordScript.DoubleKill = assetBundle.LoadAsset<AudioClip>("Double_Kill.mp3");
+            energySwordScript.TripleKill = assetBundle.LoadAsset<AudioClip>("Triple_Kill.mp3");
+            energySwordScript.Overkill = assetBundle.LoadAsset<AudioClip>("Overkill.mp3");
+            energySwordScript.Killtacular = assetBundle.LoadAsset<AudioClip>("Killtacular.mp3");
+            energySwordScript.Killtrocity = assetBundle.LoadAsset<AudioClip>("Killtrocity.mp3");
+            energySwordScript.Killamanjaro = assetBundle.LoadAsset<AudioClip>("Killamanjaro.mp3");
+            energySwordScript.Killtastrophe = assetBundle.LoadAsset<AudioClip>("Killtastrophe.mp3");
+            energySwordScript.Killpocalypse = assetBundle.LoadAsset<AudioClip>("Killpocalypse.mp3");
+            energySwordScript.Killionaire = assetBundle.LoadAsset<AudioClip>("Killionaire.mp3");
             energySwordScript.Betrayal = assetBundle.LoadAsset<AudioClip>("Betrayal.mp3");
             energySwordScript.KillingSpree = assetBundle.LoadAsset<AudioClip>("Killing_Spree.mp3");
             energySwordScript.KillingFrenzy = assetBundle.LoadAsset<AudioClip>("Killing_Frenzy.mp3");
@@ -270,11 +290,14 @@ namespace YourThunderstoreTeam.patch.Items
         {
             EnemyAICollisionDetect? enemyAICollision;
             PlayerControllerB? player;
+            float currentTime = Time.time;
+            float timeBetweenLastKill = currentTime - _lastKillTimestamp;
 
             rayHit.transform.TryGetComponent(out enemyAICollision);
             rayHit.transform.TryGetComponent(out player);
 
             await Task.Delay(200);
+
             bool isEnemyDead = false;
             bool isAlly = false;
 
@@ -289,10 +312,42 @@ namespace YourThunderstoreTeam.patch.Items
             if (isEnemyDead)
             {
                 _killCount++;
-                Console.WriteLine("Kill count: {0}", _killCount);
-
+                _tempKillCount = timeBetweenLastKill <= 5 ? _tempKillCount + 1 : 1;
+                _lastKillTimestamp = currentTime;
+                
                 if (isAlly)
                     _announcerQueue.Enqueue(Betrayal);
+
+                switch(_tempKillCount)
+                {
+                    case 2:
+                        _announcerQueue.Enqueue(DoubleKill);
+                        break;
+                    case 3:
+                        _announcerQueue.Enqueue(TripleKill);
+                        break;
+                    case 4:
+                        _announcerQueue.Enqueue(Overkill);
+                        break;
+                    case 5:
+                        _announcerQueue.Enqueue(Killtacular);
+                        break;
+                    case 6:
+                        _announcerQueue.Enqueue(Killtrocity);
+                        break;
+                    case 7:
+                        _announcerQueue.Enqueue(Killamanjaro);
+                        break;
+                    case 8:
+                        _announcerQueue.Enqueue(Killtastrophe);
+                        break;
+                    case 9:
+                        _announcerQueue.Enqueue(Killpocalypse);
+                        break;
+                    case 10:
+                        _announcerQueue.Enqueue(Killionaire);
+                        break;
+                }
 
                 switch(_killCount)
                 {
@@ -336,6 +391,11 @@ namespace YourThunderstoreTeam.patch.Items
                         _announcerQueue.Dequeue();
                 }  
             }
+        }
+
+        private void AnnounceMsg(string msg)
+        {
+            HUDManager.Instance.AddTextToChatOnServer(msg);
         }
     }
 }
