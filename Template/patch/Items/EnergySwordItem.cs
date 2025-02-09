ef24481656a654304731c438c7b273daaa9b94c2
DawnThyUltra <marion38361@gmail.com>
2025-02-09 10:29:38 -0500

    added "\n"s at the end of the item desc

--- Template/patch/Items/EnergySwordItem.cs
+++ Template/patch/Items/EnergySwordItem.cs
@@ -14,7 +14,7 @@ namespace YourThunderstoreTeam.patch.Items
     public class EnergySwordItem : GrabbableObject
     {
         private const int PRICE = 300;
-        private const string DESC = "A sword with a blade made of plasma used by high-ranking Covenant Elites. Allows the user to lunge from a distance towards their target to deliver a high-damage slash.";
+        private const string DESC = "A sword with a blade made of plasma used by high-ranking Covenant Elites. Allows the user to lunge from a distance towards their target to deliver a high-damage slash.\n\n";
 
         private const int _layerMask = 1084754248;
         private RaycastHit[] _scannedObjects;
