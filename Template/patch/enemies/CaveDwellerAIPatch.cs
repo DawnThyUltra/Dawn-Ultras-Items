1a0a68630020365842c33656a4ec0cc7e951c8b1
DawnThyUltra <marion38361@gmail.com>
2025-02-15 11:37:20 -0500

    Made maneaters weak to blades of plasma

---
+++ Template/patch/enemies/CaveDwellerAIPatch.cs
@@ -0,0 +1,23 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using YourThunderstoreTeam.patch.Items;
+
+namespace YourThunderstoreTeam.patch.enemies
+{
+    [HarmonyPatch(typeof(CaveDwellerAI))]
+    public class CaveDwellerAIPatch
+    {
+        [HarmonyPatch("HitEnemy", MethodType.Normal)]
+        [HarmonyPrefix]
+
+        private static bool OnHitEnemy(ref CaveDwellerAI __instance, int force = 1, PlayerControllerB playerWhoHit = null, bool playHitSFX = false, int hitID = -1)
+        {
+            Console.WriteLine("Hit ID: " + hitID);
+            if (hitID == EnergySwordItem.HitId)
+                __instance.KillEnemyOnOwnerClient();
+
+            return true;
+        }
+    }
+}
