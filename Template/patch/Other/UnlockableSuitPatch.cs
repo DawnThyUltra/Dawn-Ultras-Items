70a38fc19e0fb778f2b1b008033bbc96cdb2f184
DawnThyUltra <marion38361@gmail.com>
2025-03-15 15:26:15 -0400

    added a patch so that you can't change outfits when invisible

---
+++ Template/patch/Other/UnlockableSuitPatch.cs
@@ -0,0 +1,16 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace YourThunderstoreTeam.patch;
+
+
+[HarmonyPatch(typeof(UnlockableSuit))]
+public class UnlockableSuitPatch
+{
+    [HarmonyPatch(nameof(UnlockableSuit.SwitchSuitForPlayer), MethodType.Normal)]
+    [HarmonyPrefix]
+    private static bool OnSwitchSuitForPlayer(UnlockableSuit __instance, PlayerControllerB player)
+    {
+        return player is not null && !PlayerControllerBPatch.IsPlayerInvisible(player);
+    }
+}
