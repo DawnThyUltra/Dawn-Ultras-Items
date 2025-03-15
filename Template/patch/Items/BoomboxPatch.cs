521af4ed3c74ec98e46b63f8439666d98e16953e
DawnThyUltra <marion38361@gmail.com>
2025-03-15 19:51:43 -0400

    Added some HEAT (but copyright) songs

---
+++ Template/patch/Items/BoomboxPatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace YourThunderstoreTeam.patch.Items
+{
+    [HarmonyPatch(typeof(BoomboxItem))]
+    public class BoomboxPatch
+    {
+        [HarmonyPatch(nameof(BoomboxItem.Start))]
+        [HarmonyPostfix]
+        private static bool OnStart(ref BoomboxItem __instance)
+        {
+            List<AudioClip> songs = new List<AudioClip>(__instance.musicAudios);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                AudioClip currentSong = Plugin.DawnUltrasItemsAssets.LoadAsset<AudioClip>(string.Format("Song{0}", i.ToString()));
+                songs.Add(currentSong);
+            }
+
+            __instance.musicAudios = songs.ToArray();
+            return true;
+        }
+    }
+}
